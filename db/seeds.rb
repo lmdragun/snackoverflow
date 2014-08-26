e7d368f9dcfde5ae76e6db5f1ee64e3d339b907c
Jesse Shawl <jesse@jshawl.com>
2014-08-26 19:21:08 -0400

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Question.create([
- { title:'Rails Template is Missing', body:'I keep getting this error and I don\'t know what\'s wrong. :(' }
+ { title:'Rails Template is Missing', body:'I keep getting this error and I don\'t know what\'s wrong. :(' },
+ { title:'Ruby: Method Argument Error', body:"I'm not sure what this means and/or how to fix it." },
+ { title:'fatal: Not a git repository (or any of the parent directories): .git', body:'This happens when I run `git status`' }
 ])
