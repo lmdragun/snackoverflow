bf1fe37c5f8e078271f9e9801f87a69239c2ee2a
Jesse Shawl <jesse@jshawl.com>
2014-08-26 19:39:06 -0400

    add answe seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Question.create([
+Question.delete_all
+questions = Question.create([
  { title:'Rails Template is Missing', body:'I keep getting this error and I don\'t know what\'s wrong. :(' },
  { title:'Ruby: Method Argument Error', body:"I'm not sure what this means and/or how to fix it." },
  { title:'fatal: Not a git repository (or any of the parent directories): .git', body:'This happens when I run `git status`' }
 ])
+
+Answer.delete_all
+Answer.create([
+  { body:'Did you create a view file in app/views/ ? ', question:questions[0] },
+  { body:'You need to define a local variable', question:questions[1] }
+])
+
