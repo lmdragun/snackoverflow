2f80e0a1ab340d31ef9286782ecdeea0afbc271d
Jesse Shawl <jesse@jshawl.com>
2014-08-26 21:44:48 -0400

    remove answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,38 +8,4 @@ class QuestionsController < ApplicationController
     @question = Question.find( params[:id] )
   end
 
-  def new
-    @question = Question.new
-  end
-
-  def create
-    @question = Question.new( question_params )
-    if @question.save
-      redirect_to @question
-    end
-  end
-
-  def destroy
-    @question = Question.find( params[:id] )
-    @question.destroy
-    redirect_to questions_path
-  end
-
-  def edit
-    @question = Question.find( params[:id] )
-  end
-
-  def update
-    @question = Question.find( params[:id] )
-    if @question.update( question_params )
-      redirect_to @question
-    end
-  end
-
-  private
-
-  def question_params
-    params.require(:question).permit(:title, :body)
-  end
-
 end
